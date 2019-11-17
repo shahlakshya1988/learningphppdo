852c29312daa963e88bb60c3481ab0adcd173a51
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-11-17 17:05:56 +0530

    Lecture 2 Connecting Database

---
+++ TestingConnection.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * this file will require the db.php file where our connection is present
+ * if there is error nothing will work otherwise we are getting the details
+ * of this page
+ */
+require_once "db.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing The Connection</title>
+</head>
+<body>
+    <h1>Connection Successful</h1>
+</body>
+</html>
---
+++ db.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is used for connecting to the database MYSQL
+ * learningphppdo
+ */
+$dbname = "learningphppdo";
+$dbhost="localhost";
+$dbuser="root";
+$dbpass="";
+try{
+    /*
+     * having port and charset it optional
+     * required are hostname, dbname
+     * remote access may require port,
+     * charset may be omited
+     */
+    $dsn="mysql:host={$dbhost};port=3306;dbname={$dbname};charset=utf8";
+    $dsn="mysql:host={$dbhost};dbname={$dbname}";
+    $connection = new PDO($dsn,$dbuser,$dbpass);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(Exception $e){
+    echo $e->getMessage();
+    die("<br>Db Error </br>");
+}
