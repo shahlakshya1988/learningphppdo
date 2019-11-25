41c1a0315156151eb7c7b0d1c4506f79f4ce921a
Lakshya Shah <shahlakshya1988@gmail.com>
2019-11-25 17:45:07 +0530

    Lecture 16 Passing Array

---
+++ PDOPassingArray.php
@@ -0,0 +1,100 @@
+<?php
+require_once "db.php";
+$sql = "SELECT * FROM `emp_record`";
+$stmt = $connection->prepare($sql);
+$stmt->execute();
+if(isset($_GET["search"])){
+    $ename = "%".trim($_GET["ename"])."%";
+    $salary = trim($_GET["salary"]);
+    //$ename = $connection->quote($ename);
+    //$salary = $connection->quote($salary);
+    $sql = "SELECT * FROM `emp_record` where `ename` like :ename and `salary` >= :employee_salary";
+    $stmt = $connection->prepare($sql);
+
+    $stmt->execute([":ename"=>$ename,":employee_salary"=>$salary]);
+
+}
+try{
+
+
+
+    var_dump($stmt->errorInfo());
+    $Execute = $stmt->fetchAll();
+    //var_dump($Execute);
+    $numberofrows = count($Execute);
+
+}catch(Exception $e){
+    echo $e->getMessage();
+    die();
+}
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SQLInjection</title>
+</head>
+<body>
+<div>
+    <form action="" method="GET">
+        <fieldset>
+            <legend>Search For Employee</legend>
+            <div>
+                <lable for="name">
+                    Name
+                </lable>
+                <input type="text" name="ename" id="name" placeholder="Name">
+            </div>
+            <div>
+                <lable for="name">
+                    Salary Greater Than
+                </lable>
+                <select name="salary" id="salary">
+                    <option value="5000">5000</option>
+                    <option value="10000">10000</option>
+                    <option value="50000">50000</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="search" name="search">
+            </div>
+        </fieldset>
+    </form>
+</div>
+<div>
+    <?php if(isset($Execute)){ ?>
+        <table style="width:100%;">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Employee Name</th>
+                <th>SSN</th>
+                <th>Department</th>
+                <th>Salary</th>
+                <th>Home Address</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($Execute as $employee){ ?>
+                <tr>
+                    <td><?=$employee["id"]; ?></td>
+                    <td><?=$employee["ename"]; ?></td>
+                    <td><?=$employee["ssn"]; ?></td>
+                    <td><?=$employee["dept"]; ?></td>
+                    <td><?=$employee["salary"]; ?></td>
+                    <td><?=$employee["homeaddress"]; ?></td>
+                </tr>
+            <?php } ?>
+            <?php if($numberofrows<=0){?>
+                <tr>
+                    <td colspan="6">Please Search With Other Name Or Salary</td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    <?php } ?>
+</div>
+</body>
+</html>
